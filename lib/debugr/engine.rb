c6532146995d12f9671adcecc52d8db13d1fb8be
Alvin Masaba <alvinmasaba@Alvins-MacBook-Air.local>
2025-08-12 18:17:44 -0400

    Implements engine placeholder.

---
+++ lib/debugr/engine.rb
@@ -0,0 +1,13 @@
+module Debugr
+  class Engine
+    def initialize(session)
+      @session = session
+    end
+
+    # Temporary start stub: will be replaced with real TracePoint code.
+    def start
+      puts 'Engine.start called. (TracePoint is not implemented yet.)'
+      yield
+    end
+  end
+end
