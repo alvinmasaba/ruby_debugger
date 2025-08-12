9e93d2a0163d2708181bafe282f4fc9e7b46168a
Alvin Masaba <alvinmasaba@Alvins-MacBook-Air.local>
2025-08-12 18:23:30 -0400

    Added breakpoints placeholder file. Implemented test_script. Commented out breakpoint import and references in session.rb in order to run test scripts.

--- lib/debugr/engine.rb
+++ lib/debugr/engine.rb
@@ -6,7 +6,7 @@ module Debugr
 
     # Temporary start stub: will be replaced with real TracePoint code.
     def start
-      puts 'Engine.start called. (TracePoint is not implemented yet.)'
+      puts "Engine.start called. (TracePoint is not implemented yet.)"
       yield
     end
   end
---
+++ test_script.rb
@@ -0,0 +1,4 @@
+puts "This is is a test script."
+x = 1
+y = 1
+puts "x + y = #{x + y}"
